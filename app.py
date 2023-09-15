c87d9a381e67dc2aee12712647849f8d130e0b1b
Agriaya <agriaya.saxena@gmail.com>
2023-09-15 16:53:21 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request 
+import requests
+app = Flask(__name__)
+@app.route('/')
+def main():
+	return render_template("index.html")
+@app.route('/',methods=['POST'])
+
+def math_operations():
+	equation = request.form['text']
+	operation = request.form['operation']
+	api_url = 'https://newton.now.sh/api/v2//'+operation+'/'+equation
+	data = requests.get(api_url).json()
+	answer = data['result']
+	return render_template("index.html", result=answer, equation=equation)
+if __name__ == "__main__":
+	app.run()
+		
